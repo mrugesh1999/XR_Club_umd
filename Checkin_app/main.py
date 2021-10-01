af147d1e973987b94b91a10f4b241c051ecbc77a
Mrugesh1999 <34119053+mrugesh1999@users.noreply.github.com>
2021-10-01 16:28:50 -0400

    Add files via upload

--- Checkin_app/main.py
+++ Checkin_app/main.py
@@ -9,6 +9,8 @@ creds = ServiceAccountCredentials.from_json_keyfile_name('xr-club-checking-691d8
 client = gspread.authorize(creds)
 
 sheet = client.open("XR Club checkin'").worksheet("checkin' data")
+done_sheet = client.open("XR Club checkin'").worksheet("complete trades")
+
 
 sheet.update_cell(1, 1, "ID_Number")
 sheet.update_cell(1, 2, "Time")
@@ -28,7 +30,38 @@ while True:
                 value = value + word
         string = "=MATCH(" + value + ",A2:A" + i + ",0)"
         sheet.update_cell(i_int, 4, string)
-        if sheet
+        try:
+            reference = int(sheet.cell(i_int, 4).value)
+            flag = True
+        except:
+            flag = False
+        if flag:
+            print("Second time swipe")
+            sheet.update_cell(reference+1, 1, '')
+            now_out = datetime.now()
+            current_time_out = now_out.strftime("%H:%M:%S")
+            today_out = date.today()
+            today_out = str(today_out)
+            current_time_in = sheet.cell(reference+1, 2)
+            print(current_time_in)
+            current_time_in = str(current_time_in)
+            print(current_time_in)
+            today_in = sheet.cell(reference+1, 3)
+            position = done_sheet.cell(9, 9).value
+            print(type(position))
+            position = int(position)
+            today_in = str(today_in)
+
+            # today_in = today_in[len(to)]
+            done_sheet.update_cell(position, 1, value)
+            done_sheet.update_cell(position, 2, current_time_in)
+            done_sheet.update_cell(position, 3, current_time_out)
+            done_sheet.update_cell(position, 4, today_in)
+            done_sheet.update_cell(position, 5, today_out)
+            sheet.update_cell(reference+1, 1, '')
+            done_sheet.update_cell(9, 9, str(position+1))
+            flag = False
+            continue
         sheet.update_cell(i_int, 1, value)
         now = datetime.now()
         current_time = now.strftime("%H:%M:%S")
